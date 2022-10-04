bd34236ecc441d57dbd3b4e42138d85205d387fc
Jonathas Santos <jonathas4102@hotmail.com>
2022-10-04 00:43:33 -0300

    imcompleto2

---
+++ AcademiaExerciciosmatriz5/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace AcademiaExerciciosmatriz5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Questao 01
+            //int[,] matriz = new int[5, 3];
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Informe um valor para a coluna 0: ");
+            //    matriz[i, 0] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    matriz[i, 1] = matriz[i, 0] + 10;
+            //    matriz[i, 2] = matriz[i, 0] * 2;
+            //}
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for (int j = 0; j < 3; j++)
+            //    {
+            //        Console.Write("[" + i + "," + j + "] = " + matriz[i, j] + "  ");
+
+
+
+            ////Questao02
+            //int[,] matriz = new int[3, 3];
+            //int somalinha0 = 0, somacoluna0 = 0;
+            //int somalinha1 = 0, somacoluna1 = 0;
+            //int somalinha2 = 0, somacoluna2 = 0;
+
+            //for (int i = 0; i < 3; i++)
+
+            //{
+
+            //    for (int j = 0; j < 3; j++)
+            //    {
+            //        Console.WriteLine("informe um valor para a linha " + i + ",da coluna: " + j);
+            //        matriz[i, j] = int.Parse(Console.ReadLine());
+
+            //        if (i == 0)
+            //        {
+            //            somalinha0 = somalinha0 + matriz[i, j];
+            //        }
+            //        if (i == 1)
+            //        {
+            //            somalinha1 = somalinha1 + matriz[i, j];
+            //        }
+            //        if (i == 2)
+            //        {
+            //            somalinha2 = somalinha2 + matriz[i, j];
+            //        }
+
+
+            //        if (j == 0)
+            //        {
+            //            somacoluna0 = somacoluna0 + matriz[i, j];
+            //        }
+            //        if (j == 1)
+            //        {
+            //            somacoluna1 = somacoluna1 + matriz[i, j];
+            //        }
+            //        if (j == 2)
+            //        {
+            //            somacoluna2 = somacoluna2 + matriz[i, j];
+            //        }
+            //    }
+              
+            //}
+            //Console.Clear();
+            //for (int i = 0; i < 3; i++)
+
+            //{
+
+            //    for (int j = 0; j < 3; j++)
+            //    {
+            //        Console.Write(matriz[i, j] + " || ");
+            //    }
+            //    Console.WriteLine();
+
+            //}
+            //        Console.WriteLine("Soma das linhas:\n " + "Linha 0: " + somalinha0 + "  Linha 1  " + somalinha1 + "  Linha 2  " + somalinha2);
+            //Console.WriteLine("Soma das colunas:\n " + "Coluna 0: " + somacoluna0 + "  Coluna 1: " + somacoluna1 + "  Coluna 2: " + somacoluna2);
+
+        }
+    }
+}
